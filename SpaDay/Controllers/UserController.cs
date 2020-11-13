89c02576c2384ca6153d6aa8e5a9f943bc08eb72
Aquil-Babis <aquil0_0@yahoo.com>
2020-11-12 21:44:25 -0600

    Finished Core

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/submit")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.error = "";
+            if (verify != newUser.Password)
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                ViewBag.error = "The password did not verify.";
+                return View("Add");
+            }
+            else
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+        }
+    }
+}
