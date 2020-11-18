497ac8df561ba08fc56af9128473792d5dde62bf
Aquil-Babis <aquil0_0@yahoo.com>
2020-11-17 21:03:29 -0600

    Core Completed

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5-15 characters.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Invalid password.  Must have 6-20 characters.")]
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Verification required.")]
+        [Compare("Password", ErrorMessage = "Verification does not match Password.")]
+
+        public string Verify { get; set; }
+    }
+}
